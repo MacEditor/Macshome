e9951b169ab19acf91caf59df98024e638a700ff
unknown <junmo1225@daum.net>
2020-11-04 15:28:38 +0900

    study_cafe_layout

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 $(document).ready(function(){
   // 창업문의 이동
-  $(window).scroll(function(){
-    var scroll_position = $(window).scrollTop(),
-    quick_position = $('#quick_menu').css("top");
-    $('#quick_menu').css({
-      'top' : scroll_position + 200 + 'px',
-    });
+  $(window).on('scroll',function(){
+    var quick_menu = document.querySelector('#quick_menu');
+      var scroll_position = $(window).scrollTop();
+      $(quick_menu).css({
+        'top' : scroll_position + 200 + 'px'
+      });
+      console.log(scroll_position);
   });
 
   // 개인정보 수집이용 동의 활성화
   let p_popup_btn = document.querySelector('.policybtn'),
   p_popup = document.getElementById('policy_popup'),
-  p_popup_close = document.querySelector('.p_popup_close'),
-  p_close = () => {
-    p_popup.classList.add('hidden');
+  p_popup_layout = document.querySelector('.policy_layout'),
+  p_popup_close = document.querySelector('.p_popup_close');
+
+  p_popup_layout.onclick = function(){
+    p_popup.classList.remove('show');
   }
-  p_open = () => {
-    p_popup.classList.remove('hidden');
+  p_popup_close.onclick = function(){
+    p_popup.classList.remove('show');
   }
-  p_popup_close.addEventListener("click", p_close);
-  p_popup_btn.addEventListener("click", p_open);
+  p_popup_btn.onclick = function(){
+    p_popup.classList.add('show');
+  }
 });
