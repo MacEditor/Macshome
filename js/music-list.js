b79bab611780194cbee0f3c3e8ef808db9ec6ade
MacEditor <junmo1225@daum.net>
2022-01-18 12:27:12 +0900

    player

---
+++ js/music-list.js
@@ -0,0 +1,44 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "baby i'm falling (Lyrics) ft. Groundfold",
+    artist: "Teqkoi",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Honesty",
+    artist: "Pink Sweat$",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "drunk (Lyrics) feat. Laeland & Rxseboy",
+    artist: "Fudasca",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Let Me Down Slowly",
+    artist: "Alec Benjamin",
+    img: "music-4",
+    src: "music-4"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
